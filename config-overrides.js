86c3d42b77c3503560767b3700c9caa7dde24b7d
范铁男 <fantienan@forestar.com.cn>
2020-05-25 17:09:40 +0800

    feat: 修改别名

--- config-overrides.js
+++ config-overrides.js
@@ -24,7 +24,7 @@ module.exports = {
         addDecoratorsLegacy(),
         addWebpackAlias({
             /* eslint-disable */
-            '@': path.resolve(__dirname, 'src/'),
+            '@': path.resolve(__dirname, 'src'),
             /* eslint-enable */
         })
     ),
